5b3fb3bed38090560596ba9f934a0424c2a4e230
[Mac] euihyunee <lobil7782@gmail.com>
2023-08-01 15:26:21 +0900

    [ADD] Java 수업 내용 (While문)

---
+++ Acorn_BackEnd/AcronBackEnd/src/StudyWhile.java
@@ -0,0 +1,18 @@
+public class StudyWhile {
+    public static void main(String[] args) {
+
+
+        int[] iEvenOddConntArr = {0,0};
+
+        int iWhileValue = 1;
+        while (iWhileValue <= 30) {
+            System.out.println(iWhileValue);
+            if(iWhileValue % 2 == 0){
+                iEvenOddConntArr[0]++;
+            }else iEvenOddConntArr[1]++;
+            iWhileValue++;
+        }
+        System.out.println(iEvenOddConntArr[0]);
+        System.out.println(iEvenOddConntArr[1]);
+    }
+}
