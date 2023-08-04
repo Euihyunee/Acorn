9c5ab54ccc6c59dcc53add886a937afca1c966d9
euihyuneee <lobil7782@gmail.com>
2023-08-04 12:28:19 +0900

    [ADD] Sort 알고리즘 Insert(삽입 정렬), Quick(퀵 정렬), Select(선택 정렬) 코드

--- Acorn_BackEnd/AcronBackEnd/src/logic/MaxValueAlg.java
+++ Acorn_BackEnd/AcronBackEnd/src/logic/MaxValueAlg.java
@@ -3,14 +3,14 @@ package logic;
 public class MaxValueAlg {
     public static void main(String[] args) {
         int [] scores = { 60, 50, 95, 80, 70 };
-        int maxValue = max (scores);
+        int maxValue = maxValue(scores);
         System.out.println("Max Value = " + maxValue);
 
-        int minValue = min(scores);
+        int minValue = minValue(scores);
         System.out.println("Min Value = " + minValue);
 
     }
-    public static int max(int [] arrays) {
+    public static int maxValue(int [] arrays) {
     // Maximum initialization value is 0
         for (int i = 0; i < arrays.length - 1; i++) {
             if (arrays[i] > arrays[i + 1]) { // swap
@@ -19,26 +19,34 @@ public class MaxValueAlg {
                 arrays[i + 1] = temp;
             }
         }
+        printArray(arrays);
+
         int maxValue = arrays[arrays.length - 1];
         return maxValue;
     }
-    public static int min(int [] arrays) {
-        // Maximum initialization value is 0
-        for (int i = 0; i < arrays.length - 1; i++) {
-            if (arrays[i] < arrays[i + 1]) { // swap
-                int temp = arrays[i];
-                arrays[i] = arrays[i + 1];
-                arrays[i + 1] = temp;
+    public static int minValue(int [] arrays) {
+        // Maximum initialization value is 0\
+        for(int j = 0 ; j< arrays.length; j++){
+            for (int i = 0; i < (arrays.length - 1 - j); i++) {
+                if (arrays[i] < arrays[i + 1]) { // swap
+                    int temp = arrays[i];
+                    arrays[i] = arrays[i + 1];
+                    arrays[i + 1] = temp;
+                }
             }
         }
-        int maxValue = arrays[arrays.length - 1];
-        return maxValue;
+
+        printArray(arrays);
+
+        int minValue = arrays[arrays.length - 1];
+        return minValue;
     }
     public static void printArray(int [] arrays){
         System.out.print("배열 : ");
         for (int num : arrays) {
             System.out.print(num + " ");
         }
+        System.out.println();
     }
 }
 
