b228b86b8138d37cb889a5a2377aa20fa1eee249
DESKTOP-BM6JR4B\hbi <euihyunee@github.com>
2023-08-28 17:29:07 +0900

    [ADD] Basket Project -> Main문 전단계

--- Acorn_BackEnd/AcronBackEnd/src/acorn_project/Basket.java
+++ Acorn_BackEnd/AcronBackEnd/src/acorn_project/Basket.java
@@ -1,18 +1,48 @@
 package acorn_project;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class Basket {
-	
-	private long id;
-	private List<Product> productList;
-	
-	
-	void productDelete() {}
-	  
-	void productRead() {}
-	
-	void productUpdate() {}
-	
-	void productAdd() {}
+
+
+    private  HashMap<Long, BasketProduct> hashProduct
+     = new HashMap<Long, BasketProduct>();
+    
+    
+    // 장바구니 목록을 모두 띄움
+    public void BasketRead() {
+    	if(hashProduct != null) {
+    		for(Long key : hashProduct.keySet()) {
+        		System.out.println(hashProduct.get(key).toString());
+        	}
+    	}else {
+    		System.out.println("장바구니가 터엉~");
+    	}
+    	
+    }
+    // 사용자가 상품을 선택했을 때 장바구니에 추가하는 메서드
+    public void BasketAdd(Product product) {
+    	hashProduct.put(product.getterProductId(), 
+    			new BasketProduct(product));
+    	System.out.println("선택하신 품목이 추가되었습니다.");
+    }
+    // 사용자가 장바구니에서 특정 품목을 삭제하는 메서드
+    public void BasketDelete(Long key) {
+    	System.out.println(hashProduct.get(key).toString() +
+    			" -> 삭제합니다. ");
+    	hashProduct.keySet().remove(key);
+    	System.out.println("삭제 완료했습니다.");
+    }
+    // OrderCheck() 메서드에서 사용할 것
+    // 장바구니 품목의 모든 가격을 계산해서 반환해줌 
+    // type -> Long
+    public Long BasketTotalPrice() {
+    	Long totalPrice = 0L;
+    	for(Long key : hashProduct.keySet()) {
+    		totalPrice += hashProduct.get(key).getTotalPrice();
+    	}
+    	return totalPrice;
+    }
+
+
 }
--- Acorn_BackEnd/AcronBackEnd/src/acorn_project/Product.java
+++ Acorn_BackEnd/AcronBackEnd/src/acorn_project/Product.java
@@ -1,10 +1,40 @@
 package acorn_project;
 
 public class Product {
-	
-	private long id;
-	private String name;
-	private int num;
-	private long price;
-	private String category;
+
+    private  Long ProductId;
+    private  String ProductName;
+    private  Long Price;
+
+    public Product() {
+    }
+    public Product(Long productId, String productName, Long price) {
+		ProductId = productId;
+		ProductName = productName;
+		Price = price;
+	}
+	public Product(Product product) {
+    	this.ProductId =product.getterProductId();;
+    	this.ProductName =product.getterProductName();;
+    	this.Price =product.getterPrice();; 
+    }
+    public Long getterProductId() {
+    	return ProductId;
+    }
+    public String getterProductName() {
+    	return ProductName;
+    }
+    public Long getterPrice() {
+    	return Price;
+    }
+    
+    @Override
+    public String toString() {
+    	return String.format(this.ProductId + ". "
+    			+"상품명 : " 
+    			+ this.ProductName 
+    			+ ", 가격 : " + this.Price + "원"
+    			);
+    }
+
 }
